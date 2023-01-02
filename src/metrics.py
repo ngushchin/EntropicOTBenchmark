05bc87229fdbef4a8f4503073a46483dae421bff
ngushchin <guschin1998@gmail.com>
2023-01-02 16:37:48 +0300

    BW-UVP is added

--- src/metrics.py
+++ src/metrics.py
@@ -1,8 +1,14 @@
 import torch
 import numpy as np
 from .tools import freeze
+from scipy.linalg import sqrtm
 
 import gc
+
+
+def symmetrize(X):
+    return np.real((X + X.T) / 2)
+
 
 def score_fitted_maps(benchmark, D, D_conj, size=4096):
     '''Estimates L2-UVP and cosine metrics for transport map by the gradients of potentials.'''
@@ -109,3 +115,25 @@ def MMD(x, y, kernel, device, scale_factor):
             XY += torch.exp(-0.5*dxy/a)
 
     return torch.mean(XX + YY - 2. * XY)
+
+
+def compute_BW_UVP(model_samples, true_samples):
+    model_samples_covariance = np.cov(model_samples.T)
+    model_samples_mu = model_samples.mean(axis=0)
+    model_samples_covariance_sqrt = symmetrize(sqrtm(model_samples_covariance))
+    
+    true_samples_covariance = np.cov(true_samples.T)
+    true_samples_mu = true_samples.mean(axis=0)
+    true_samples_covariance_sqrt = symmetrize(sqrtm(true_samples_covariance))
+
+    mu_term = 0.5*((true_samples_mu - model_samples_mu)**2).sum()
+    covariance_term = (
+        0.5*np.trace(model_samples_covariance) + 
+        0.5*np.trace(true_samples_covariance) -
+        np.trace(symmetrize(sqrtm(model_samples_covariance_sqrt@true_samples_covariance@model_samples_covariance_sqrt)))
+    )
+
+    BW = mu_term + covariance_term
+    BW_UVP = 100*(BW/(0.5*np.trace(true_samples_covariance)))
+        
+    return BW_UVP

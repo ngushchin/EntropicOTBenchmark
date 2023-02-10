9132a0b0d82ca630795b8c3868ac341daaf7fe77
Nikita Gushchin <gushchin1998@gmail.com>
2023-02-10 15:24:02 +0000

    fix

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
-from setuptools import setup, find_packages
-from os.path import join, dirname
+from setuptools import setup
+
+packages = {
+    'eot_benchmark': 'benchmark/'
+}
 
 setup(
-    name='eot_benchmark',
-    version='1.0',
-    packages=find_packages(),
-#     long_description=open(join(dirname(__file__), 'README.txt')).read(),
+    name="eot_benchmark",
+    version='0.1',
+    author="Gushchin Nikita (thesinsay)",
+    license="MIT",
+    description="""""",
+    packages=packages,
+    package_dir=packages,
+    include_package_data=False,
 )

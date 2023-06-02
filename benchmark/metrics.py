41f9646d3e56da621ca1a1c8726fcfede6f25b2b
n.gushchin <n.gushchin@vt11.zhores>
2023-06-02 19:40:57 +0300

    cond bw is added

--- benchmark/metrics.py
+++ benchmark/metrics.py
@@ -13,6 +13,32 @@ def symmetrize(X):
     return np.real((X + X.T) / 2)
 
 
+def compute_BW_with_gt_stats(model_samples, true_samples_mu, true_samples_covariance):
+    model_samples_covariance = np.cov(model_samples.T)
+    model_samples_mu = model_samples.mean(axis=0)
+    model_samples_covariance_sqrt = symmetrize(sqrtm(model_samples_covariance))
+    
+    true_samples_covariance_sqrt = symmetrize(sqrtm(true_samples_covariance))
+
+    mu_term = 0.5*((true_samples_mu - model_samples_mu)**2).sum()
+    covariance_term = (
+        0.5*np.trace(model_samples_covariance) + 
+        0.5*np.trace(true_samples_covariance) -
+        np.trace(symmetrize(sqrtm(model_samples_covariance_sqrt@true_samples_covariance@model_samples_covariance_sqrt)))
+    )
+
+    BW = mu_term + covariance_term
+        
+    return BW
+
+
+def compute_BW_by_gt_samples(model_samples, true_samples):
+    true_samples_covariance = np.cov(true_samples.T)
+    true_samples_mu = true_samples.mean(axis=0)
+        
+    return compute_BW_with_gt_stats(model_samples, true_samples_mu, true_samples_covariance)
+
+
 def compute_BW_UVP_with_gt_stats(model_samples, true_samples_mu, true_samples_covariance):
     model_samples_covariance = np.cov(model_samples.T)
     model_samples_mu = model_samples.mean(axis=0)
